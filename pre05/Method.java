6d97f588dc087120449e771c83a5600a075b57ef
AASINE CASSARA <85954161+acassara13@users.noreply.github.com>
2022-05-07 00:18:36 +0000

    pre05

--- pre05/Method.java
+++ pre05/Method.java
@@ -25,6 +25,25 @@ public class Method {
 		}
 	}
 // Question 4 Write a method called ack that takes two ints as parameters and that computes and returns the value of the Ackermann function.
-  
+  public static int ack(int m, int n) {
+		if (m == 0) {
+			return n + 1;
+		}
+		else if (n == 0) {
+			return ack(m - 1, 1);
+		}
+		else {
+			return ack(m - 1, ack(m, n - 1)); 
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("A(0, 0) = " + ack(0, 0));
+		System.out.println("A(0, 1) = " + ack(0, 1));
+		System.out.println("A(1, 0) = " + ack(1, 0));
+		System.out.println("A(2, 1) = " + ack(2, 1));
+		System.out.println("A(1, 1) = " + ack(1, 1));
+		System.out.println("A(2, 2) = " + ack(2, 2));
+	}
 }
    
