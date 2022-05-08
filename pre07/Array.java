4778094bb5a33d7a8e87a6e36b294786e52f9eb5
AASINE CASSARA <85954161+acassara13@users.noreply.github.com>
2022-05-08 00:28:14 +0000

    almost done

--- pre07/Array.java
+++ pre07/Array.java
@@ -6,6 +6,8 @@ public class Array{
          powArray(a,0);
         for(double value:a){
             System.out.println(value);
+
+        
         }
 
     }
@@ -17,5 +19,48 @@ public class Array{
         }
     }
 
-    public static void histogram(int b[])
+    public static int[] histogram(int[] array, int count){
+      int[] counter = new int[count];
+      for (int element:array){
+         counter[element]++;
+      }
+      return counter;
+  
+    public static int indexOfMax(int[] numbers){
+      int index = 0;
+      for (int i=1;i<numbers.length;i++){
+         if (numbers[i] > numbers[index]){
+            index = i;
+         }
+      }
+      return index;
+   }
+//enhanced loop
+   public static int indexOfMaxEL(int[] num]){
+      int count = 0;
+      int i = 0;
+      for (int num:nums){
+         if (nums[i] > nums[count]){
+            count = i;
+         }
+         i++;
+      }
+      return count;
+   }  
+  public static boolean[] sieve(int n){
+      boolean[] result = new boolean[n];
+      for (int i=2;i<n;i++)
+         result[i] = true;
+      for (int number=2;number<=(int)Math.sqrt(n);number++){
+         if (result[number] == true){
+            for (int listIndex = number*number;listIndex < n;listIndex += number){
+               result[listIndex] = false;
+            }
+         }
+      }
+      return result;
+   }
+      }
 }
+
+    
